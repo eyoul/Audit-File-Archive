a30173a54dc7b392356e5b9cce3d1c8be8fd757d
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-10-10 04:09:52 -0700

    Summery

--- trash/config.py
+++
@@ -1,7 +0,0 @@
-# Flask app configuration
-DEBUG = True
-SECRET_KEY = 'DEV'
-
-# SQLAlchemy database configuration
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:&CorSrvPa&111@localhost/knowlage'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
